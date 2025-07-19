f1cb7f47c87d4809b963c7843e60ca3d827585d9
mohanty14 <45348870+mohanty14@users.noreply.github.com>
2025-07-19 20:32:27 +0530

    messge

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { chromium, defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config={
+  testDir: './tests',
+ timeout:30 *100,
+ expect:{
+  timeout:50*100
+ },
+
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browsername:'chromium',
+    headless:false,
+  
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+};
+module.exports=config;
---
+++ tests/UIButtonsInteraction.spec.js
@@ -0,0 +1,23 @@
+
+import { test, expect,  Page } from '@playwright/test';
+
+
+test("error message capture",async({page})=>{
+
+   await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+   const username=page.locator('#username')
+   const password=page.locator('#password')
+const signinbutton = page.locator('input[type="submit"]');
+ await signinbutton.click();
+ console.log(await page.locator("[style*='block']").textContent())
+}
+)
+test.only("dropdown check",async({page})=>{
+ await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+ const drpdown=page.locator('select.form-control')
+ await drpdown.selectOption("Teacher")
+
+ await page.locator('input#usertype').last().click();
+ await page .locator('#okayBtn').click();
+ await page.pause();
+})
