21f26f6a35122fd16fcb9f190f99066669d55127
sunanda <sunanda.mohanty93@gmail.com>
2025-07-20 12:46:34 +0530

    login for application

--- tests/UIButtonsInteraction.spec.js
+++ tests/UIButtonsInteraction.spec.js
@@ -12,7 +12,7 @@ const signinbutton = page.locator('input[type="submit"]');
  console.log(await page.locator("[style*='block']").textContent())
 }
 )
-test.only("dropdown check",async({page})=>{
+test("dropdown check",async({page})=>{
  await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
  const drpdown=page.locator('select.form-control')
  await drpdown.selectOption("Teacher")
@@ -20,5 +20,11 @@ test.only("dropdown check",async({page})=>{
  await page.locator('input#usertype').last().click();
  await page .locator('#okayBtn').click();
  expect(await page.locator('input#usertype').last()).toBeChecked();
+page.locator('#terms').click();
+expect(await page.locator('#terms')).toBeChecked();
+await page.locator('#terms').uncheck();
+expect(await page.locator('#terms').toBeChecked()).toBeFalsy();
+    
+
  await page.pause();
 })
